2f4b4e98bcf266644244b9744a3793e2f8e0ac19
Elise643 <83870589+Elise643@users.noreply.github.com>
2024-12-02 09:25:48 -0600

    Add files via upload

---
+++ DevImageJoinFiles.bat
@@ -0,0 +1,7 @@
+REM *** Build Adv CS Dev Image FROM C:\Lanschool Files ***
+
+
+IF NOT EXIST "LinuxMint22C-CS.zip" c:\"Program Files"\7-Zip\7z x c:\"LanSchool Files"\LinuxMint22C-CS_2.7z.001 -y
+
+dir /ad
+pause
